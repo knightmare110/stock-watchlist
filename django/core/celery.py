6df7c33a54b3d81bb814549cfea5404d9bcf678d
knight110 <knightmare.protagonist@gmail.com>
2024-11-26 08:05:44 -0500

    Fix issues

--- django/core/celery.py
+++ django/core/celery.py
@@ -28,7 +28,7 @@ app.conf.beat_schedule = {
 }
 
 # Set up the SNS client and Redis client
-sns_client = boto3.client('sns', region_name='us-east-1')
+sns_client = boto3.client('sns', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), region_name='us-east-1')
 redis_client = redis.Redis(host=os.getenv('REDIS_HOST', 'localhost'), port=6379, db=0)
 # redis_client.flushdb()  # For the current database
 
